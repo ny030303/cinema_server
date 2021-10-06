c4b2c27e1214fa1e4fac59fb912e4e2db7bd70dc
jny <ny030303@naver.com>
2021-10-06 16:36:09 +0900

    multer s3 연결 + upload 부분

--- CommenUtil.js
+++ CommenUtil.js
@@ -1,7 +1,7 @@
 exports.getNowDateToYYMMDD =  () => {
-    let d = new Date();
-    let fomatDate = new Date(d.getTime() - (d.getTimezoneOffset() * 60000)).toISOString();
-    return fomatDate.replace("T", " ").slice(0, -5);
+  let d = new Date();
+  let fomatDate = new Date(d.getTime() - (d.getTimezoneOffset() * 60000)).toISOString();
+  return fomatDate.replace("T", " ").slice(0, -5);
 }
 
 /* ==== Local ==== */
@@ -12,14 +12,6 @@ exports.getNowDateToYYMMDD =  () => {
 // });
 // exports.formDataUpload = multer({storage: storage});
 /* ============== */
-
-// const multerS3 = require('multer-s3');
-// const multer = require('multer');
-// const path = require('path');
-// const AWS = require('aws-sdk');
-
-
-
 const multer = require('multer');
 const multerS3 = require('multer-s3');
 const aws = require('aws-sdk');
@@ -28,76 +20,73 @@ const dotenv = require('dotenv');
 dotenv.config(); // env
 // aws.config.loadFromPath(__dirname + '/../config/s3.json');
 aws.config.update({
-  "accessKeyId": process.env.AWS_ACCESS_KEY_ID,
-  "secretAccessKey": process.env.AWS_SECRET_ACCESS_KEY,
-  "region" : "ap-northeast-2"
+"accessKeyId": process.env.AWS_ACCESS_KEY_ID,
+"secretAccessKey": process.env.AWS_SECRET_ACCESS_KEY,
+"region" : "ap-northeast-2"
 });
 
 const s3 = new aws.S3();
+
 const upload = multer({
-    storage: multerS3({
-        s3: s3,
-        contentType: multerS3.AUTO_CONTENT_TYPE,
-        bucket: 'cinema-s3-upload',
-        acl: 'public-read',
-        key: function(req, file, cb){
-          console.log(file);
-            cb(null, Date.now() + '.' + file.originalname.split('.').pop()); // 이름 설정
-        }
-    })
+  storage: multerS3({
+      s3: s3,
+      contentType: multerS3.AUTO_CONTENT_TYPE,
+      bucket: 'cinema-s3-upload/posters',
+      acl: 'public-read',
+      key: function(req, file, cb){
+        console.log(file);
+          cb(null, Date.now() + '.' + file.originalname.split('.').pop()); // 이름 설정
+      }
+  })
 });
 exports.formDataUpload = upload;
+exports.awsS3 = s3;
 
 
-// const upload = multer({
-//   storage: multerS3({
-//       s3: AWS,
-//       bucket: "cinema-s3-upload", // 버킷 이름
-//       contentType: multerS3.AUTO_CONTENT_TYPE, // 자동을 콘텐츠 타입 세팅
-//       acl: 'public-read', // 클라이언트에서 자유롭게 가용하기 위함
-//       key: (req, file, cb) => {
-//           console.log(file);
-//           let extension = path.extname(file.originalname);
+/* ==== chrome load ==== */
 
-//           console.log(extension);
-//           cb(null, Date.now().toString() + extension)
-//       },
-//   }),
-//   limits: { fileSize: 5 * 1024 * 1024 }, // 용량 제한
-// });
+const chrome = require('selenium-webdriver/chrome');
+exports.loadChromeDriver = async (extOptions = []) => {
+  const defaultOptions = [
+    '--headless',
+    '--disable-dev-shm-usage',
+    '--window-size=1280,960',
+    '--no-sandbox', // required for Linux without GUI
+    '--disable-gpu' // required for Windows,
+  ];
+  extOptions = (typeof extOptions === 'string') ? [extOptions] : extOptions;
+  extOptions.forEach(opt => defaultOptions.push(opt));
 
-
-
-// const upload = require('../modules/multer');
-
-exports.formDataUpload = upload;
-
+  const options = new chrome.Options();
+  defaultOptions.forEach(opt => options.addArguments(opt));
+  const builder = new Builder().forBrowser('chrome').setChromeOptions(options);
+  return (await builder.build());
+}
 
 
 
-// ---------
-
+/* ==== genore filter ==== */
 
 let {dbQuery, init} = require('./controllers/dbController');
 exports.filterGenoreAndInsertToTable = async () => {
-  await init();
-  let res = await dbQuery("GET", "SELECT DISTINCT genore FROM `movie`", []);
-  let returnArr = [];
-  res.row.forEach(json => {
-    json.genore.split(',').forEach(word => {
-      let nowWord = word.trim();
-      let inWord = returnArr.find(v => v == nowWord);
-      if(inWord == undefined && nowWord.trim().length > 0) returnArr.push(nowWord);
-    });
+await init();
+let res = await dbQuery("GET", "SELECT DISTINCT genore FROM `movie`", []);
+let returnArr = [];
+res.row.forEach(json => {
+  json.genore.split(',').forEach(word => {
+    let nowWord = word.trim();
+    let inWord = returnArr.find(v => v == nowWord);
+    if(inWord == undefined && nowWord.trim().length > 0) returnArr.push(nowWord);
   });
-  returnArr.forEach(async keyword => {
-    let res = await dbQuery("GET", "SELECT * FROM movie_genore WHERE keyword = ?", [keyword]);
-    let isIn = res.row[0];
-    console.log(isIn);
-    if(!isIn) {
-      let insertRes = await dbQuery("INSERT", "INSERT INTO movie_genore VALUES (?)", [keyword]);
-      console.log(insertRes);
-    }
-  });
-  // console.log(returnArr);
+});
+returnArr.forEach(async keyword => {
+  let res = await dbQuery("GET", "SELECT * FROM movie_genore WHERE keyword = ?", [keyword]);
+  let isIn = res.row[0];
+  console.log(isIn);
+  if(!isIn) {
+    let insertRes = await dbQuery("INSERT", "INSERT INTO movie_genore VALUES (?)", [keyword]);
+    console.log(insertRes);
+  }
+});
+// console.log(returnArr);
 }
--- dockerfile
+++ dockerfile
@@ -1,22 +1,45 @@
-FROM node:12.18.0
+FROM ubuntu:18.04
 
-ENV DOCKERIZE_VERSION v0.6.1
-RUN wget https://github.com/jwilder/dockerize/releases/download/$DOCKERIZE_VERSION/dockerize-linux-amd64-$DOCKERIZE_VERSION.tar.gz \
-    && tar -C /usr/local/bin -xzvf dockerize-linux-amd64-$DOCKERIZE_VERSION.tar.gz \
-    && rm dockerize-linux-amd64-$DOCKERIZE_VERSION.tar.gz
+RUN apt update && apt upgrade -y
+RUN apt install -y curl 
+RUN apt install -y wget
+RUN apt install -y unzip
+
+RUN apt-get install -y default-jre
+RUN apt-get -f install
+
+ENV CHROME_VERSION "94.0.4606.61"
+
+COPY google-chrome-stable_94.0.4606.71-1_amd64.deb .
+COPY dockerize-linux-amd64-v0.6.1.tar.gz .
+
+RUN apt-get install -y ./google-chrome-stable_94.0.4606.71-1_amd64.deb
+RUN tar -C /usr/local/bin -xzvf dockerize-linux-amd64-v0.6.1.tar.gz
+
+RUN wget https://chromedriver.storage.googleapis.com/$CHROME_VERSION/chromedriver_linux64.zip \
+    && unzip chromedriver_linux64.zip \
+    && mv chromedriver /usr/bin/chromedriver \
+    && chown root:root /usr/bin/chromedriver \
+    && chmod +x /usr/bin/chromedriver
+
+RUN apt-get install -y xvfb
+
+RUN curl -sL https://deb.nodesource.com/setup_14.x | bash \
+    && apt-get install -y nodejs \
+    && curl -L https://www.npmjs.com/install.sh | sh
 
 RUN npm install -g nodemon
-RUN mkdir -p /usr/src/app
+RUN npm install -g yarn
 
-WORKDIR /usr/src/app
-
-COPY . .
-RUN npm install yarn
+RUN mkdir -p /usr/lib
+WORKDIR /usr/lib
+COPY package.json .
 RUN yarn install
 
-RUN chmod +x docker-entrypoint.sh  
-ENTRYPOINT ./docker-entrypoint.sh
+WORKDIR /usr/src/app
+COPY . .
 
-RUN tar -xvf ./T.tar -C ./public/images
+RUN chmod +x ./docker-entrypoint.sh
+ENTRYPOINT ./docker-entrypoint.sh
 
 EXPOSE 8080
